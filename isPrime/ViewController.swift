d1d1abfe4acb1151c93d8f82f16c71919ada0726
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-03-25 18:52:27 -0700

    Basic Swift App
    Application that determines whether or not the number entered is prime.
    Must be run on Xcode.

--- isPrime/ViewController.swift
+++ isPrime/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var PrimeBar: UITextField!
+    @IBOutlet weak var isPrime: UILabel!
+    
+    
+    @IBAction func Correct(sender: AnyObject) {
+        
+        if let myPrime = Int(PrimeBar.text!){
+        isPrime.text =  String(myPrime)+" is a Prime"
+        
+        if (myPrime != 2 && myPrime != 1){
+        
+        for( var i = 2; i < myPrime; i++)
+        {
+            if( myPrime%i == 0)
+            {
+                isPrime.text = String(myPrime)+" is not a Prime"
+            }
+        }
+    
+        }
+        }
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
